403c74c22a3d06facd2b4c3d0ddbdcd6e5145229
videvirral-eng <videvirral@gmail.com>
2026-07-13 03:46:48 +0700

    Update config_vip.py

--- config_vip.py
+++ config_vip.py
@@ -8,49 +8,56 @@ VIP_PACKAGES = {
         "id": "vip1",
         "name": "💎 VIP 1 Hari",
         "days": 1,
-        "price": 15000,
+        "price": 20000,
     },
 
     "vip3": {
         "id": "vip3",
         "name": "💎 VIP 3 Hari",
         "days": 3,
-        "price": 25000,
+        "price": 30000,
     },
 
     "vip5": {
         "id": "vip5",
         "name": "💎 VIP 5 Hari",
         "days": 5,
-        "price": 35000,
+        "price": 40000,
     },
 
     "vip7": {
         "id": "vip7",
         "name": "💎 VIP 7 Hari",
         "days": 7,
-        "price": 45000,
+        "price": 50000,
     },
 
     "vip10": {
         "id": "vip10",
         "name": "💎 VIP 10 Hari",
         "days": 10,
-        "price": 50000,
+        "price": 65000,
     },
 
-    "vip20": {
-        "id": "vip20",
-        "name": "💎 VIP 20 Hari",
-        "days": 20,
-        "price": 70000,
+    "vip15": {
+        "id": "vip15",
+        "name": "💎 VIP 15 Hari",
+        "days": 15,
+        "price": 80000,
     },
 
     "vip30": {
         "id": "vip30",
-        "name": "💎 VIP 30 Hari",
+        "name": "💎 VIP 1 Bulan",
         "days": 30,
-        "price": 100000,
+        "price": 120000,
+    },
+
+    "vip60": {
+        "id": "vip60",
+        "name": "💎 VIP 2 Bulan",
+        "days": 60,
+        "price": 220000,
     },
 
 }
