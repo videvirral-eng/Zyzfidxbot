1416c870fd607a95d12ad33d035181a19b375709
videvirral-eng <videvirral@gmail.com>
2026-07-13 03:44:51 +0700

    Update config_market.py

--- config_market.py
+++ config_market.py
@@ -1,9 +1,32 @@
-MARKET_FEE = 10          # persen
-
-MIN_WITHDRAW = 50000
-
 PAYMENT_PROVIDER = "bayargg"
 
-MARKET_NAME = "Decoder Market"
+MARKET_NAME = "ZYXFIDX Market"
 
-AUTO_APPROVE_WITHDRAW = False
+# =========================
+# UPLOAD PERMISSION
+# =========================
+ADMIN_CAN_UPLOAD = True
+VIP_CAN_UPLOAD = True
+USER_CAN_UPLOAD = False
+
+# =========================
+# VIP PACKAGES
+# =========================
+VIP_PACKAGES = {
+    "1 Hari": 1,
+    "3 Hari": 3,
+    "5 Hari": 5,
+    "7 Hari": 7,
+    "10 Hari": 10,
+    "15 Hari": 15,
+    "1 Bulan": 30,
+    "2 Bulan": 60,
+}
+
+# =========================
+# MARKET
+# =========================
+ALLOW_SINGLE_FILE_SALE = True   # File eceran
+ALLOW_FOLDER_SALE = True        # Folder/Paket
+ALLOW_FREE_FILE = True          # File gratis
+ALLOW_PAID_FILE = True          # File berbayar
